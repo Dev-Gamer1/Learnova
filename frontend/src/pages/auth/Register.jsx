e1c4997fbd62d31586ef8e56de114836c0e4e323
hyperxgamerz216-ops <hyperxgamerz216@gmail.com>
2025-12-17 22:59:05 +0530

    added new file

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/auth.css";
+
 
 const Register = () => {
-  return (
-    <div className="auth-container">
-      <h2>Create Student Account</h2>
+return (
+<div className="auth-container">
+<div className="auth-box">
+<h2>Create Student Account</h2>
 
-      <form>
-        <input type="text" placeholder="Full Name" />
-        <input type="email" placeholder="Email address" />
-        <input type="password" placeholder="Password" />
 
-        <button type="submit">Sign Up</button>
-      </form>
+<form>
+<input type="text" placeholder="Full Name" />
+<input type="email" placeholder="Email address" />
+<input type="password" placeholder="Password" />
+<button type="submit">Sign Up</button>
+</form>
 
-      <Link to="/">Already have an account? Login</Link>
-    </div>
-  );
+
+<div className="links" style={{ justifyContent: "center" }}>
+<Link to="/">Already have an account? Login</Link>
+</div>
+</div>
+</div>
+);
 };
+
 
 export default Register;
